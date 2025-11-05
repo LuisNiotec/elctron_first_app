922a1de02ad0d4e784f633394dc855d1bf22a911
LuisNiotec <luis@niotec.es>
2025-11-05 12:30:32 +0100

    Añadida vista

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log("Hello from Electron 👋");
+const { app, BrowserWindow } = require("electron");
+
+// Para typeScript, para la info sobre tipos
+//const { app, BrowserWindow } = require("electron/main");
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    //fullscreen: true
+  });
+
+  win.loadFile("index.html");
+};
+
+app.whenReady().then(() => {
+  createWindow();
+});
